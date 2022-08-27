63c48dc0e88c271762c0d4c087ac02cd396c1910
Kachii <kachikaduru@gmail.com>
2022-08-27 01:15:44 +0100

    responsive

--- Day 4 Moble/script.js
+++ Day 4 Moble/script.js
@@ -6,12 +6,16 @@ const toggleNav = function() {
     if(nav.classList.contains('open')) {
         nav.classList.remove("open");
         menu.style.width = "2.7rem";
+        exit.style.width = "0";
         menu.style.overflow = "auto";
+        menu.style.overflow = "hidden";
         
     } else{
         nav.classList.add('open');
         menu.style.width = "0";
+        exit.style.width = "4rem";
         menu.style.overflow = "hidden";
+        exit.style.overflow = "auto";
     };
 };
 
